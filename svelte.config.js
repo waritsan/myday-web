117b3667109acb2755b8f5e6d45243851ff9152d
Warit Santaputra <waritsan@gmail.com>
2026-02-11 18:59:30 +0700

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			out: 'build'
-		})
+			out: 'build',
+			precompress: false,
+			strict: false
+		}),
+		prerender: {
+			entries: ['/chat']
+		}
 	}
 };
 
